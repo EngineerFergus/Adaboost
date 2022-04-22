74588672197ff613c817d4e3b79c67a2b9eea14d
williamskei97 <williamskei97@gmail.com>
2022-04-21 22:25:10 -0400

    Removed console due to naming issues. Added classes for data reading.

---
+++ Adaboost/CSVData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adaboost
+{
+    internal class CSVData
+    {
+        public string[] Headers { get; set; }
+        public string[]? LabelNames { get; set; }
+        public double[][] Features { get; set; }
+        public int[] Labels { get; set; }
+
+        public CSVData(string[] headers, double[][] features, int[] labels, string[]? labelNames)
+        {
+            Headers = headers;
+            Labels = labels;
+            Features = features;
+            LabelNames = labelNames;
+        }
+    }
+}
---
+++ Adaboost/CSVReader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adaboost
+{
+    internal class CSVReader
+    {
+        public static CSVData ReadCSV(string dir)
+        {
+            List<string> headers = new List<string>();
+            List<List<double>> features = new List<List<double>>();
+            List<int> labels = new List<int>();
+            List<string> labelNames = new List<string>();
+
+            using (FileStream stream = File.OpenRead(dir))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    int count = 0;
+
+                    while (!reader.EndOfStream)
+                    {
+                        string? line = reader.ReadLine();
+                        if (line == null) { continue; }
+                        string[] splits = line.Split(',');
+                        int numCols = splits.Length;
+
+                        if (count == 0)
+                        {
+                            for (int i = 0; i < numCols - 1; i++)
+                            {
+                                features.Add(new List<double>());
+                                headers.Add(splits[i]);
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 0; i < splits.Length - 1; i++)
+                            {
+                                _ = double.TryParse(splits[i], out double f);
+                                features[i].Add(f);
+                            }
+
+                            labelNames.Add(splits[splits.Length - 1]);
+                        }
+
+                        count++;
+                    }
+                }
+            }
+
+            var distinctLabels = labelNames.Distinct().ToList();
+
+            foreach (string label in labelNames)
+            {
+                for (int i = 0; i < distinctLabels.Count(); i++)
+                {
+                    if (label == distinctLabels[i])
+                    {
+                        labels.Add(i);
+                        break;
+                    }
+                }
+            }
+
+            double[][] fArr = features.Select(x => x.ToArray()).ToArray();
+
+            return new CSVData(headers.ToArray(), fArr, labels.ToArray(), labelNames.ToArray());
+        }
+    }
+}
