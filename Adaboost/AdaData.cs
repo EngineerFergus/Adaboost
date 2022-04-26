6e1578bd9f0205ce05eb77fd08548435ab95fe9a
williamskei97 <williamskei97@gmail.com>
2022-04-26 18:14:28 -0400

    Added labeled data classes for easier maintenance of data.

---
+++ Adaboost/AdaData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adaboost
+{
+    public class AdaData : LabeledData
+    {
+        public AdaData(double[] features, int Label) : base(features, Label)
+        {
+            if(Label != -1 && Label != 1)
+            {
+                throw new ArgumentException($"Cannot create instance of {nameof(AdaData)} with label " +
+                    $"value that is not -1 or 1.");
+            }
+        }
+    }
+}
---
+++ Adaboost/LabeledData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adaboost
+{
+    public class LabeledData
+    {
+        private double[] features;
+        public int Label { get; }
+
+        public double this[int index]
+        {
+            get
+            {
+                return features[index];
+            }
+        }
+
+        public LabeledData(double[] features, int label)
+        {
+            Label = label;
+            this.features = features;
+        }
+
+        public int[] ToOneHot(int numClasses)
+        {
+            if(numClasses <= Label)
+            {
+                throw new ArgumentException($"Exception in {nameof(ToOneHot)}: Cannot convert to " +
+                    $"one hot encoding when label is greater than total number of classes.");
+            }
+
+            if(Label < 0)
+            {
+                throw new Exception($"Exception in {nameof(ToOneHot)}: Cannot convert to one hot " +
+                    $"encoding with a label less than zero.");
+            }
+
+            int[] oneHots = new int[numClasses];
+            oneHots[Label] = 1;
+            return oneHots;
+        }
+    }
+}
